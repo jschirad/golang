b3b88dcc36713ed1c0ae24ca46e51c9cf09918c3
Facundo  Schirado <jfacuscirado@gmail.com>
2022-08-27 10:38:19 +0200

    Variables en Go

--- src/main.go
+++ src/main.go
@@ -5,4 +5,31 @@ import "fmt"
 func main() {
 	// Hola Mundo
 	fmt.Println("Hola Mundo!")
+
+	// Declaracion de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println("pi ", pi)
+	fmt.Println("pi2", pi2)
+
+	// Declaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(area, altura, base)
+
+	// Declaracion de variables Zero
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area Cuadrado ", areaCuadrado)
 }
