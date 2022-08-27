3d0aec638483891d4f8ba015acc94497d41e2f44
Facundo  Schirado <jfacuscirado@gmail.com>
2022-08-27 11:41:13 +0200

    operadores en go

---
+++ src/operadores.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Area cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area Cuadrado ", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	//Suma
+	result := x + y
+	fmt.Println("Suma ", result)
+
+	// Resta
+	result = y - x
+	fmt.Println("Resta ", result)
+
+	// Multiplicación
+	result = x * y
+	fmt.Println("Multiplicación ", result)
+
+	// División
+	result = y / x
+	fmt.Println("División ", result)
+
+	// Modulo
+	result = y % x
+	fmt.Println("Modulo ", result)
+
+	// Incremental
+	x++
+	fmt.Println("x++ ", x)
+
+	// Decremental
+	x--
+	fmt.Println("x--", x)
+}
