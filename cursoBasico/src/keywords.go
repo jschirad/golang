b913dc5f5c291ab37f99edee5286f4ad4a961a89
Facundo  Schirado <jfacuscirado@gmail.com>
2022-08-28 19:57:07 +0200

    switches and keywords

--- cursoBasico/src/keywords.go
+++ cursoBasico/src/keywords.go
@@ -4,6 +4,20 @@ import "fmt"
 
 func main() {
 	// Defer - ultima tarea de la función
-	defer fmt.Println("Hola!")
-	fmt.Println("Mundo!")
+	defer fmt.Println("Adios!")
+	fmt.Println("Hola Mundo!")
+
+	// Continue
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		if i == 2 {
+			fmt.Println("Es 2")
+			continue
+		}
+		// Break
+		if i == 8 {
+			fmt.Println("Break")
+			break
+		}
+	}
 }
