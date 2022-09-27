307b801e897a0cd27c0ba26888e567cf1ce0d665
Facundo  Schirado <jfacuscirado@gmail.com>
2022-09-27 21:54:05 +0200

    added logging middleware

--- cursoPractico/Proyecto4-Servidor/middleware.go
+++ cursoPractico/Proyecto4-Servidor/middleware.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
+
+// Middleware Template
+// func MiddlewareName() Middleware {
+// 	return func(f http.HandlerFunc) http.HandlerFunc {
+// 		// Acceso al escritor y al request -
+// 		return func(w http.ResponseWriter, r *http.Request) {
+// 			// Logica del Middleware
+
+// 		}
+// 	}
+// }
 
 func CheckAuth() Middleware {
 	return func(f http.HandlerFunc) http.HandlerFunc {
@@ -18,6 +31,21 @@ func CheckAuth() Middleware {
 			} else {
 				return
 			}
+		}
+	}
+}
+
+func Logging() Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		// Acceso al escritor y al request -
+		return func(w http.ResponseWriter, r *http.Request) {
+			// Logica del Middleware
+			start := time.Now()
+			defer func() {
+				log.Println(r.URL.Path, time.Since(start))
+			}()
+			// Siguiente Middleware
+			f(w, r)
 		}
 	}
 }
